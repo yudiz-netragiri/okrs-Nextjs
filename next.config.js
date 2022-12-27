2f0e5021cf45b1aa18ba1af96415507265d7cc38
yudiz_netragiri_goswami <netragiri.goswami@yudiz.com>
2022-12-27 18:26:21 +0530

    latest commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'crictracker-admin-panel-local-dev-08032022.s3.ap-south-1.amazonaws.com',
+        port: '',
+        pathname: '/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
