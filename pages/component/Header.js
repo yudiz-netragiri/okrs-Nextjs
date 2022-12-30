18889ec868d7a8de4704ee2655b2155c7307bf1c
yudiz_netragiri_goswami <netragiri.goswami@yudiz.com>
2022-12-30 15:44:08 +0530

    add sub article

--- pages/component/Header.js
+++ pages/component/Header.js
@@ -1,17 +1,32 @@
 import { useQuery } from '@apollo/client'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { GET_ALL_HEADERDATA } from '../../graphql/api'
+import logo from '../../assets/image/logo.svg'
 
 function Header () {
   const { data } = useQuery(GET_ALL_HEADERDATA)
 
-  return <section className='main-nav mt-2'> <div className='container header-comps'>
-    {
-    data?.getMenuTree?.aResults?.map((i, index) => {
-      return <Link href="#" key={index} className='main-links'>{i.sTitle}</Link>
-    })}</div>
+  return (
+    <section className="main-nav ">
+      <div className='cric-head'>
+        <div className='container'>
+        <Image src={logo}/>
+        </div>
+      </div>
+      {' '}
+      <div className="container header-comps">
+        {data?.getMenuTree?.aResults?.map((i, index) => {
+          return (
+            <Link href="#" key={index} className="main-links">
+              {i.sTitle}
+            </Link>
+          )
+        })}
+      </div>
     </section>
+  )
 }
 
 export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client'
+import Link from 'next/link'
 import React from 'react'
 import { GET_ALL_ARTICLE } from '../graphql/api'
 
@@ -14,8 +15,8 @@ function index () {
             <div key={index}>
               <div className="container row">
                 <div className="col-8 mx-auto">
-                  <div className="text-center">
-              <h5 className="text-center">{i.sName}</h5>
+                  <div className="text-center my-3 white-div ">
+                    <h5 className="text-center">{i.sName}</h5>
                     <img
                       className="main-Img"
                       src={`https://crictracker-admin-panel-local-dev-08032022.s3.ap-south-1.amazonaws.com/${
@@ -23,17 +24,28 @@ function index () {
                           .oImg.sUrl
                       }`}
                     />
+                    <h5 className='text-start my-3'>
+                    {
+                      i.aArticle.find((mainArt) => mainArt.sType === 'nBig')
+                        .sTitle
+                    }
+                  </h5>
                   </div>
-                    <h5>
-                      {
-                        i.aArticle.find((mainArt) => mainArt.sType === 'nBig')
-                          .sTitle
-                      }
-                    </h5>
-                    <div className='row'>
-                <div className='col-4'>asfasdjkfldks</div>
-                <div className='col-8'>asfasdjkfldks</div>
-                </div>
+
+                    {i.aArticle
+                      .filter((mainArt) => mainArt.sType !== 'nBig')
+                      .map((article, index) => {
+                        return (<div key={index} className="row my-4 white-div ">
+                            <div className="col-3 small-article-div my-2">
+                              <img className='small-article' src={`https://crictracker-admin-panel-local-dev-08032022.s3.ap-south-1.amazonaws.com/${article.oImg.sUrl}`}></img>
+                            </div>
+                            <div className="col-9 main-div">
+                            <Link href="/posts/first-post" className='article-heading'>  <h5> {article?.sTitle}</h5></Link>
+                              <div className='bottom-div'><p className='date'>{article.dPublishDate}</p></div>
+                              </div>
+                              </div>
+                        )
+                      })}
                 </div>
               </div>
             </div>
